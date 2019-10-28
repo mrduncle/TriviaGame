49e1c827411a2d72960881acdd5303c8db1003fd
mrduncle1 <dun__can@hotmail.com>
2019-10-28 19:43:47 +0800

    Qquestion, correct answer, incorrect answer and all answers extracted from api response and added to an array with correct answer added to a random location in the all answers sub array. bBasic display element for question and answers added in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,101 @@
 //@ts-check
 
+let timer = 29;
+let timerDisplay;
+let arrData = [];
+let numQuestions = 5;
+let questionsAsked = 0;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
-function getQuestions(response) {
-    // $("#questions").text(JSON.stringify(response));
-    
-    $.each(response, function(i, qn) {
-        console.log(qn[1])
-        let attList = ["category", "question", "correct_answer", "incorrect_answers"];
-        $.each(attList, function(j, attbt) {
-            $("#questions").append("<p>" + attbt + ": " + qn[attbt] + "</p>");
-        })
+let queryURL = "https://opentdb.com/api.php?amount=" + numQuestions;
+
+function randomNum() {
+    //find a random number between 0 and 3 which determines the position
+    //that the correct answer will be inserted into the incorrect answers
+    //array to yield an all answers array
+    let randNo = Math.floor(Math.random() * (4))
+    return randNo;
+}
+
+//ensures that there are no repeats in the random order array randOrder
+// function randomQuestOrder(randOrder,index) {
+//     let orderVal = randomNum();
+//     while (randOrder.includes(orderVal)) {
+//         orderVal = randomNum();
+//     }
+//     randOrder[index] = orderVal;
+//     console.log(randOrder)
+//     return randOrder;
+// }
+
+//displays the question and then the answers in a random order
+function showQuestions(qaData) {
+    $("#question").text(qaData[0]);
+    $.each(qaData[3], function(i, anAnswer){
+        $("#answer" + i).attr("value", anAnswer);
     })
+    // $.each(randOrder, function(index, value) {
+    //     randomQuestOrder(randOrder, index);
+    // })
+    // console.log(randOrder);
+}
+
+function getQuestionData(response) {
+    // for (let j = 0; j < numQuestions; j++) {  //repeat for the number of questions the user wishes to answer
+        questionsAsked++;
+        let questionData = response.results[0]; //change back to j
+        let attList = ["question", "correct_answer", "incorrect_answers"]
+        arrData = [];
+        //loop through the list of attributes that are contained in response.results[j]
+        $.each(attList, function(i, attbt) { 
+            arrData[i] = questionData[attbt];
+        })
+        arrData[3] = arrData[2].slice(0); //make a copy of arrData[2] (incorrect_answers)
+        let randInsert = randomNum();  
+        arrData[3].splice(randInsert, 0, (arrData[1].trim())); //add the correct answer to incorrect_answers
+                                                               // in a random location (0 to 3) to get all answers
+        console.log(arrData);
+        showQuestions(arrData);
+    // }
+}
+
+function answerCorrect() {
+    let userAnswer = $(this).attr("value");
+    if userAnswer.trim() === arrData[1].trim() {
+        $("#response-correct").text("Correct! Your score for this game is " + correctAnswers + "/" + questionsAsked + "." );
+        //display message for correct answer
+        correctAnswers++;
+
+    }
+    else {
+        $("#response-incorrect").text("Incorrect! You score for this game is " + correctAnswers + "/" questionsAsked + ".");//display message showing the correct answer
+        incorrectAnswers++;
+    }
 }
 
 
+$(document).on("click", "#submit-answer", answerCorrect);
 
-let queryURL = "https://opentdb.com/api.php?amount=50";
+//display seconds remaining -works
+// function displayCount() {
+//     $("#countdown").text(timer + "s");
+//     console.log(timer);
+//     if (timer === 0) {
+//         clearInterval(timerDisplay);
+//         timer = 29;
+//     }
+//     timer--;
+// }
+
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
-    // console.log(response);
-    getQuestions(response)
-    
-    
+    getQuestionData(response);    
 })
+
+//call setInterval for the purpose of showing a countdown on screen -works
+// timerDisplay = setInterval(displayCount, 1000); 
